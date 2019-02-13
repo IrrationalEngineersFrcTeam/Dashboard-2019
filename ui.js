edcdd64d41d7b0da223cbde8d3fa5584fa756035
unknown <matt.orie@gmail.com>
2019-02-12 21:42:34 -0500

    .

--- ui.js
+++ ui.js
@@ -1,34 +1,69 @@
+document.getElementById('connectionIndicator').innerHTML = "Check5";
+/*
 var ui = {
 
-camera: Document.getElementById('camera'),
-timer: Document.getElelementById('timer'),
-connetion: Document.getElementById('connectionIndicator'),
-minimap: Document.getElementById('minimap'),
-ctx = canvas.getContext("2d"),
+camera: document.getElementById('camera'),
+timer: document.getElelementById('timer'),
+connetion: document.getElementById('connectionIndicator'),
+minimap: document.getElementById('minimap'),
 
-}
 
+};
+
+
+document.getElementById('connectionIndicator').innerHTML = "Check4";
+*/
+/*
+NetworkTables.addRobotConnectionListener(function(immediateNotify){
+
+    document.getElementById('connectionIndicator').innerHTML = "Robot Connected";
+},true);
+*/
+/* if (NetworkTables.isWsConnected() === true){
+
+    document.getElementById('connectionIndicator').innerHTML = "Check6";
+} */
 
 
 /* not connecting for some reason, figure out tomorrow */
+
+/*NetworkTables.addKeyListener(function(key, value, isNew){
+
+      if (value === '/SmartDashboard/robotConnection'){
+
+    document.getElementById('connectionIndicator').innerHTML = "Check7";
+    
+}
+
+}, true); */
+    
+document.getElementById('connectionIndicator').innerHTML = "Check 7";
+
+
 NetworkTables.addGlobalListener(function(key , value){
+    
+     var key = NetworkTables.getKeys(key);
+    document.getElementById('connectionIndicator').innerHTML = "Check 8";
+   
+switch (key) {
 
-
-switch ('key') {
-
-    case ('/SmartDashboard/robotConnection'):
+    case '/SmartDashboard/robotConnection':
+    document.getElementById('connectionIndicator').innerHTML = "Check 10";
        if (value === true) {
-           ui.connection.innerHTML = "Robot Connected";
+        document.getElementById('connectionIndicator').innerHTML = "Robot is Connected";
        }else{
-           ui.connection.innerHTML = "No Robot Connected";
+        document.getElementById('connectionIndicator').innerHTML = "Robot is not Connected";
        }
     break;
 
+    default:
+    document.getElementById('connectionIndicator').innerHTML = "Check 9";
+/*
     case ('/SmartDashboard/timeRunning'):
     var s = 135;
 
-    if (timerunning === true) {
-        ui.timer.style.color = "#00d500";
+    if (timeRunning === true) {
+        document.getElementById('timer').innerHTML = "#00d500";
 
         var countdown = setInterval(function () {
             s--; // Subtracts one second
@@ -46,36 +81,35 @@ switch ('key') {
                 return;
             } else if (s <= 30) {
 
-                ui.timer.style.color = 'red';
+                document.getElementById('timer').color = 'red';
 
                 
                 ui.timeWarning.style.color = (s % 2 === 0) ? 'red' : 'transparent';
 
             } else if (s <= 75) {
 
-                ui.timer.style.color = 'yellow';
+                document.getElementById('timer').color = 'yellow';
             }
-            ui.timer.innerHTML = m + ':' + visualS;
+            document.getElementById('timer').innerHTML = m + ':' + visualS;
         }, 1000);
     } else {
         s = 135;
     }
     break;
-
+*/
+/*
     case ('/SmartDashboard/encoderL' , 'SmartDashboard/encoderR' , '/SmartDashboard/NavXYaw'):
     /*
     placeholder! needs actual math for translate.
     ui.ctx.fillRect(0,0,150,75);
     */
    /* get the speed of the robot based on the rotation of the encoders and convert to px. */
-    var roboRate = 2 * (Math.PI(1.9125 * (encoderL + encoderR)));
+   /* var roboRate = 2 * (Math.PI(1.9125 * (encoderL + encoderR)));
     /* sets up a vector for the robot and translates to a coordinate point on the canvas. */
-    ui.ctx.fillRect(Math.cos(NavXYaw) * roboRate , Math.sin(NavXYaw) * roboRate , 11 , 10 );
+    /*ui.ctx.fillRect(Math.cos(NavXYaw) * roboRate , Math.sin(NavXYaw) * roboRate , 11 , 10 );
     ui.ctx.fillStyle = "#FF0000";
-    break;
-
-
-
+    break;*/
+ 
 }
 
    
